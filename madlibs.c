d3ac0bf622252c1308da680df11344779664cac1
ruben magana <rubenm@unr.edu>
2024-11-25 23:46:36 +0000

    madlibs 11/25/2024

---
+++ madlibs.c
@@ -0,0 +1,45 @@
+//Header Comments
+//Authors:
+//Date:
+//Purpose:
+
+#include <stdio.h> 
+#include <stdbool.h>
+//Macros
+#define FILE_NAME "madlib1.txt"
+#define FILE_NAME2 "madlib2.txt"
+
+//Prototypes
+void madInput(FILE *x,char x, );
+void words();
+void scanWords();
+void scanFile();
+void printFile();
+
+//Main Function
+int main(){
+
+FILE *input = fopen(FILE_NAME, "r");
+FILE *input2 = fopen(FILE_NAME2, "r");
+
+//variables
+char adjective;
+char verb;
+char noun;
+
+
+
+return 0;
+}
+
+//Functions
+
+
+
+
+
+
+
+
+
+
