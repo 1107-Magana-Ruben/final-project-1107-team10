3ee6c230a38c19669813d46bd03628b1967f05c4
1107-Cabanero-Thomas <tdelacab@gmail.com>
2024-12-11 04:14:48 +0000

    organize and debugging

--- madlibs.c
+++ madlibs.c
@@ -29,53 +29,50 @@ char outline[100][100];
 int users_choice_4file;
 int word_count;
 int line_count;
-	
+
+//File Pointers:
+FILE *input;
+FILE *input2;
+
 //Code:	
 
 	explain_game_2user(&users_choice_4file);
 
-			
-
-//FILE *input;
-//FILE *input2;
 	
 		if(users_choice_4file == 1){
-			FILE *input = fopen(FILE_NAME, "r");
+			input = fopen(FILE_NAME, "r");
 			if(input == NULL){
 					printf("ERROR, I Cant read the file. shutting down\n");
 						return 0;
 				}
 		scanFile(input, outline, &line_count, &word_count);
 		fclose(input);
-		scanWords(words, word_count, outline);
-
-		printMadlib(outline, words, line_count, word_count);
 		}		
 		
 			else if(users_choice_4file == 2){
-					FILE *input2 = fopen(FILE_NAME2, "r");
+					input2 = fopen(FILE_NAME2, "r");
 					if(input2 == NULL){
-					printf("ERROR, I Cant read the file. shutting down\n");
+					printf("ERROR, I can't read the file. shutting down\n");
 						return 0;
 					}
 			scanFile(input2, outline, &line_count, &word_count);
 			fclose(input2);
-			scanWords(words, word_count, outline);
-
-			printMadlib(outline, words, line_count, word_count);
 			}
 			
-				else if (input == NULL){
-					printf("ERROR, I Cant read the file. shutting down\n");
+				else if(input == NULL){
+					printf("ERROR, I can't read the file. shutting down\n");
 						return 0;
 				}
 				
 	  				else{	
-							printf("Please enter either 1 or 2, to begin the game");
-								return 0;
-	 				  }       
+						printf("Please enter either 1 or 2, to begin the game");
+						return 0;
+	 				}       
 	   
+scanWords(words, word_count, outline);
 
+		//printMadlib(outline, words, line_count, word_count);
+		
 
 	return 0;
 }
@@ -98,38 +95,54 @@ void explain_game_2user(int *users_choice_4file){
 
 //char and int
 void scanFile(FILE *fp, char outline[][100], int *line_count, int *word_count){
-	char sentence[100];
-		while (fgets(sentence, 100, fp) != NULL){
+
+char sentence[100];
+	
+		while(fgets(sentence, 100, fp) != NULL){
+		
 			if(sentence[0] == 'A' || sentence[0] == 'N' ||sentence[0] == 'V'){
+			
 			(*word_count)++;
+			
 			}
 	(*line_count)++;
+	
 		}	
 }
 //WHAT I CHANGED(DELETE IF CHANGED): Letter is not assigned to anything
 void scanWords(char words[][WORD_CAPACITY], int word_count, char outline[][100]){
+
 	for(int i = 0; i < word_count; i++){
-		char letter, outline[i][0];
+		//char letter, outline[i][0];
 			
-			if (outline[i][0]== 'A'){
-				printf("Enter An Adjective:");
-			}else if (outline[i][0] == 'N'){
-				printf("Enter An Noun:");
-			}else if (outline[i][0] == 'V'){
-				printf("Enter An Verb:");
-			}else{
-				printf("Can't Recognize Input!!!");
+			//if(outline[i][0] == 'A'){
+				//printf("Enter An Adjective:");
+				//scanf("%s", words[i]);
+			//}
+			
+			//else if(outline[i][0] == 'N'){
+				//printf("Enter An Noun:");
+				//scanf("%s", words[i]);
+			//}
+			
+			//else if(outline[i][0] == 'V'){
+				//printf("Enter An Verb:");
+				//scanf("%s", words[i]);
+			//}
+			
+			//else{
+				//printf("Can't Recognize Input!!!");	
 				
-			scanf("%s", words[i]);	
-		}
+			//}
 	}	
-}			 
+}	
+		 
 void printMadlib(char outline[][100], char words[][WORD_CAPACITY], int line_count, int word_count){
 
 int index = 0;
 
 	for(int i = 0; i < 0; i++){
-		if(outline[i][0] == 'A'||outline[i][0] == 'V'||outline[i][0] == 'N'){
+		if(outline[i][0] == 'A'|| outline[i][0] == 'V'|| outline[i][0] == 'N'){
 			printf( "%s ", words[index++]);
 		}else{	
 			printf( "%s", outline[i]);
